c1f974618dcf36c17c9c8c2f52ee3c8e00d3b115
Barbara Laurince <blaurince@hotmail.com>
2023-05-09 14:40:37 -0400

    dotnet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+//Console.WriteLine("Hello, World!");
+
+int age = 26;
+string name = "Barbara";
+bool isHere = true;
+double salary = 22000.01;
+
+Console.WriteLine($"Hi, {name}. Your age is {age}  and is here? {isHere}");
+Console.WriteLine($"And you make 💰${salary} a year");
+
